953fb8256a2bfceee9781ff76b42a13825cdc392
wangleigang <wangleigang@funji.club>
2023-03-14 17:59:04 +0800

    动态路由

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import Layout from '@/components/layout'
 import utilStyles  from '../styles/utils.module.css'
 import { getSortedPostsData } from '@/lib/posts'
+import Date from '@/components/date'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,11 +39,11 @@ export default function Home({allPostsData}: any) {
           {
             allPostsData.map(({id, date, title}: any) => (
               <li className={utilStyles.listItem} key={id}>
-                { title }
+                <Link href={`/posts/${id}`}>{ title }</Link>
                 <br />
-                {id}
-                <br />
-                { date }
+                <small className={ utilStyles.lightText }>
+                  <Date dateString={date} />
+                </small>
               </li>
             ))
           }
